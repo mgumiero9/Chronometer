fa7023f5f9a048592d9b7bb08eb566b66a60a0d5
mgumiero9 <mgumiero9@gmail.com>
2016-11-02 20:50:51 -0200

    Attached Layout

--- app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
+++ app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
@@ -3,8 +3,13 @@ package xyz.polymath6.chronometer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button myButtonStart = (Button) findViewById(R.id.buttonStart);
+    Button myButtonStop = (Button) findViewById(R.id.buttonStop);
+    Chronometer focus = (Chronometer) findViewById(R.id.chronometer1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
