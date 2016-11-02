6c6283b19e42f5a536b8b4ae74e75c85ae369105
mgumiero9 <mgumiero9@gmail.com>
2016-11-02 21:33:06 -0200

    Creating listeners

--- app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
+++ app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
@@ -1,22 +1,86 @@
 package xyz.polymath6.chronometer;
 
-import android.content.Intent;
+
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
     Button myButtonStart = (Button) findViewById(R.id.buttonStart);
     Button myButtonStop = (Button) findViewById(R.id.buttonStop);
-    Chronometer focus = (Chronometer) findViewById(R.id.chronometer1);
+    Button myButtonRestart = (Button) findViewById(R.id.buttonRestart);
+    Button myButtonSet = (Button) findViewById(R.id.buttonSet);
+    Button myButtonClear = (Button) findViewById(R.id.buttonClear);
+    Chronometer myFocus = (Chronometer) findViewById(R.id.chronometer1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Chronometer.class);
-        startActivity(intent);
+        myButtonStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                focus.start();
+            }
+        });
+
+        myButtonStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                focus.stop();
+            }
+        });
+
+
+        myButtonRestart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                focus.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+
+        myButtonSet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                focus.setFormat("Formated Time - %s");
+            }
+        });
+
+
+        myButtonClear.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                focus.setFormat(null);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
