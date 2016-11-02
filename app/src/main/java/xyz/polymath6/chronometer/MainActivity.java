36790f31ca1485106730d358672998ade50f7c58
mgumiero9 <mgumiero9@gmail.com>
2016-11-02 14:51:53 -0200

    First commit

---
+++ app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.polymath6.chronometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
