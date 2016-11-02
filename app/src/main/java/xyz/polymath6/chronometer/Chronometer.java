0c1818160ca12f0061d4917b08ec046a6bd25808
mgumiero9 <mgumiero9@gmail.com>
2016-11-02 20:39:53 -0200

    Reseting App

---
+++ app/src/main/java/xyz/polymath6/chronometer/Chronometer.java
@@ -0,0 +1,14 @@
+package xyz.polymath6.chronometer;
+
+import android.content.Context;
+import android.widget.TextView;
+
+/**
+ * Created by mgumiero9 on 02/11/16.
+ */
+
+public class Chronometer extends TextView {
+    public Chronometer(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
+++ app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
@@ -1,7 +1,8 @@
 package xyz.polymath6.chronometer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, Chronometer.class);
+        startActivity(intent);
     }
 }
