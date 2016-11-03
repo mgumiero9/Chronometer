bcff0ced34c3268f306ed164d6ce02b3301c91c0
mgumiero9 <mgumiero9@gmail.com>
2016-11-02 22:21:47 -0200

    First working

--- app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
+++ app/src/main/java/xyz/polymath6/chronometer/MainActivity.java
@@ -1,36 +1,34 @@
 package xyz.polymath6.chronometer;
 
-
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button myButtonStart = (Button) findViewById(R.id.buttonStart);
-    Button myButtonStop = (Button) findViewById(R.id.buttonStop);
-    Button myButtonRestart = (Button) findViewById(R.id.buttonRestart);
-    Button myButtonSet = (Button) findViewById(R.id.buttonSet);
-    Button myButtonClear = (Button) findViewById(R.id.buttonClear);
-    Chronometer myFocus = (Chronometer) findViewById(R.id.chronometer1);
-
+    Button myButtonStart, myButtonStop, myButtonRestart, myButtonSet, myButtonClear;
+    Chronometer myFocus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myButtonStart = (Button) findViewById(R.id.buttonStart);
+        myButtonStop = (Button) findViewById(R.id.buttonStop);
+        myButtonRestart = (Button) findViewById(R.id.buttonRestart);
+        myButtonSet = (Button) findViewById(R.id.buttonSet);
+        myButtonClear = (Button) findViewById(R.id.buttonClear);
+        myFocus = (Chronometer) findViewById(R.id.chronometer1);
+
         myButtonStart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
-                myFocus.start();
+                 myFocus.start();
             }
         });
 
@@ -38,49 +36,38 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
                 myFocus.stop();
             }
         });
-
 
         myButtonRestart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
                 myFocus.setBase(SystemClock.elapsedRealtime());
             }
         });
-
 
         myButtonSet.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
                 myFocus.setFormat("Formated Time - %s");
             }
         });
-
 
         myButtonClear.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-
                 myFocus.setFormat(null);
             }
         });
     }
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
+    }*/
 }
